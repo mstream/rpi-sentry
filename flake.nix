11988e4eec3d3c5703d52d156b9cba980650b931
mstream <maciej.laciak@gmail.com>
2024-01-14 07:08:08 +0000

    add cross-architecture setup

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
       systemOutputs = system:
         let
           pkgs = import inputs.nixpkgs { inherit system; };
-          packages = {
+          buildPackages = pkgs: {
             app = import ./packages/app { inherit pkgs; };
             appDebug = import ./packages/app { inherit pkgs; enableDebug = true; };
             core = import ./packages/core { inherit pkgs; };
@@ -22,7 +22,7 @@
             sandboxDebug = import ./packages/sandbox { inherit pkgs; enableDebug = true; };
             tkgpio = import ./packages/tkgpio { inherit pkgs; };
           };
-          apps = {
+          buildApps = packages: {
             app = {
               type = "app";
               program = "${packages.app}/bin/app";
@@ -30,6 +30,14 @@
             appDebug = {
               type = "app";
               program = "${packages.appDebug}/bin/app";
+            };
+            app-cross-armv7l-linux = {
+              type = "app";
+              program = "${packages.app-cross-armv7l-linux}/bin/app";
+            };
+            appDebug-cross-armv7l-linux = {
+              type = "app";
+              program = "${packages.appDebug-cross-armv7l-linux}/bin/app";
             };
             sandbox = {
               type = "app";
@@ -41,10 +49,10 @@
             };
           };
         in
-        {
-          apps = {
-            inherit (apps) app appDebug sandbox sandboxDebug;
-            default = apps.sandbox;
+        rec {
+          apps = rec {
+            inherit (buildApps packages) app appDebug sandbox sandboxDebug;
+            default = sandbox;
           };
           devShell = pkgs.mkShell {
             inherit name;
@@ -53,9 +61,11 @@
               PS1="\[\e[33m\][\[\e[m\]\[\e[34;40m\]${name}:\[\e[m\]\[\e[36m\]\w\[\e[m\]\[\e[33m\]]\[\e[m\]\[\e[32m\]\\$\[\e[m\] "
             '';
           };
-          packages = {
-            inherit (packages) app appDebug core gpiozero sandbox sandboxDebug tkgpio;
-            default = packages.sandbox;
+          packages = rec {
+            inherit (buildPackages pkgs) app appDebug core gpiozero sandbox sandboxDebug tkgpio;
+            default = sandbox;
+            app-cross-armv7l-linux = (buildPackages pkgs.pkgsCross.armv7l-hf-multiplatform).app;
+            appDebug-cross-armv7l-linux = (buildPackages pkgs.pkgsCross.armv7l-hf-multiplatform).appDebug;
           };
         };
     in
